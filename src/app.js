46712650301b76e8cae5bf2ce6365c2cecb10a94
gabriel-jimenez93 <gabo0813ajm@gmail.com>
2024-09-05 04:52:52 -0500

    arreglo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+  let action = ['comió', 'pisó', 'aplastó', 'rompió'];
+  let what = ['mi tarea', 'mi teléfono', 'el auto'];
+  let when = ['antes de la clase', 'cuando dormía', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras oraba'];
+
+
+  let txt1 = math.floor(math.random()* who.length);
+  let txt2 = math.floor(math.random()* action.length);
+  let txt3 = math.floor(math.random()* what.length);
+  let txt4 = math.floor(math.random()* when.length);
+
+  document.getElementById("excuses").innerHTML = (who[txt1] + action[txt2] + what[txt3] + when[txt4]);
+
+
   console.log("Hello Rigo from the console!");
 };
